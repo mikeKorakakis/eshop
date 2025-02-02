49784afe6a399f642c27770347c625a6c4282ba3
nikxar1997 <73241831+nikxar1997@users.noreply.github.com>
2025-02-02 14:02:58 +0200

    shippin location

--- new/shop-client/components/checkout/UserInfoView/user-info-view.tsx
+++ new/shop-client/components/checkout/UserInfoView/user-info-view.tsx
@@ -19,113 +19,132 @@ import { useUI } from '@/lib/context/ui-context';
 const { link_profile_addresses, link_checkout_payment } = LINKS;
 
 type Props = {
-	dictionary: Dictionary;
-	customer: User | null;
+  dictionary: Dictionary;
+  customer: User | null;
 };
 
 interface CustomerBaseInfo {
-	firstName: string;
-	lastName: string;
-	email: string;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
 
 const UserInfoView = ({ dictionary, customer }: Props) => {
-	const common_dictionary = dictionary.common;
-	const checkout_dictionary = dictionary.checkout;
+  const common_dictionary = dictionary.common;
+  const checkout_dictionary = dictionary.checkout;
 
-	const router = useRouter();
-	const [loading, setLoading] = useState(false);
-	const { openModal, setModalView } = useUI();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { openModal, setModalView } = useUI();
 
+  const handleLogout = async () => {
+    await logout();
+    window.location.reload();
+  };
 
-
-	const handleLogout = async () => {
-		await logout();
-		window.location.reload();
-	}
-
-
-
-
-
-
-
-
-
-
-
-	return (
-		<>
-			<div>
-				<div>
-					<h2 className="text-lg font-medium text-gray-900">{checkout_dictionary.customer_info}</h2>
-					{/* {message && <ErrorMessage error={{ message }} className=" mt-4" />} */}
-					{customer?.email && (
-						<>
-							<div className="mt-8">
-								<div className="">
-									<h2 className="text-lg font-medium text-gray-900">
-										{/* {checkout_dictionary.welcome_back} */}
-									</h2>
-									<p className="mt-1 text-sm text-gray-600">
-										{checkout_dictionary.logged_in_as} {customer.username} (
-										{customer.email})
-										<button
-											className=" text-sm ml-2 text-red-500 underline"
-											type="button"
-										onClick={handleLogout}
-										>
-											{common_dictionary.logout}
-										</button>
-									</p>
-									<p className="mt-1 text-sm text-red-600">
-										{/* {checkout_dictionary.set_address}{' '}
+  return (
+    <>
+      <div>
+        <div>
+          <h2 className="text-lg font-medium text-gray-900">{checkout_dictionary.customer_info}</h2>
+          {/* {message && <ErrorMessage error={{ message }} className=" mt-4" />} */}
+          {customer?.email && (
+            <>
+              <div className="mt-8">
+                <div className="">
+                  <h2 className="text-lg font-medium text-gray-900">
+                    {/* {checkout_dictionary.welcome_back} */}
+                  </h2>
+                  <p className="mt-1 text-sm text-gray-600">
+                    {checkout_dictionary.logged_in_as} {customer.username} ({customer.email})
+                    <button
+                      className=" ml-2 text-sm text-red-500 underline"
+                      type="button"
+                      onClick={handleLogout}
+                    >
+                      {common_dictionary.logout}
+                    </button>
+                  </p>
+                  <p className="mt-1 text-sm text-red-600">
+                    {/* {checkout_dictionary.set_address}{' '}
 										<Link href={link_profile_addresses} className="text-red-500 underline">
 											{checkout_dictionary.here}
 										</Link> */}
-
-
-									</p>
-								</div>
-							</div>
-						</>
-					)}
-				</div>
-				<div>
-					{!customer && (
-						<>
-							<div className="pt-8 text-sm">
-								<span className="text-accent-7">{common_dictionary.login_to_continue}</span>
-								{` `}
-								<a
-									className="text-accent-9 cursor-pointer text-red-500 font-bold hover:underline"
-									onClick={() => {
-										setModalView('LOGIN_VIEW');
-										openModal();
-									}}
-								>
-									{common_dictionary.login}
-								</a>
-							</div>
-						</>
-					)}
-
-					<>
-
-						<Button
-							className="mt-10 h-10 w-full"
-							variant="slim"
-							type="submit"
-							loading={loading}
-							disabled={!customer}
-							onClick={() => router.push(link_checkout_payment)}
-						//   onClick={() => setStep(1)}
-						>
-							{common_dictionary.next}
-						</Button>
-					</>
-				</div>
-				{/* {isGuest && !customer && (
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+        <div>
+          {!customer && (
+            <>
+              <div className="pt-8 text-sm">
+                <span className="text-accent-7">{common_dictionary.login_to_continue}</span>
+                {` `}
+                <a
+                  className="text-accent-9 cursor-pointer font-bold text-red-500 hover:underline"
+                  onClick={() => {
+                    setModalView('LOGIN_VIEW');
+                    openModal();
+                  }}
+                >
+                  {common_dictionary.login}
+                </a>
+              </div>
+            </>
+          )}
+		  <FormInput
+            name="Address"
+            maxLength={19} // Max length including spaces (16 digits + 3 spaces)
+            type="text"
+            label={`Address`}
+            placeholder="Enter your address"
+            // error={'Dsa'}
+            onInput={() => {}} // Add formatting handler
+          />
+		  <FormInput
+            name="ZIP"
+            maxLength={19} // Max length including spaces (16 digits + 3 spaces)
+            type="text"
+            label={`ZIP`}
+            placeholder="Enter your ZIP code"
+            // error={'Dsa'}
+            onInput={() => {}} // Add formatting handler
+          />
+          <FormInput
+            name="City"
+            maxLength={19} // Max length including spaces (16 digits + 3 spaces)
+            type="text"
+            label={`City`}
+            placeholder="Enter your city"
+            // error={'Dsa'}
+            onInput={() => {}} // Add formatting handler
+          />
+		  <FormInput
+            name="Phone"
+            maxLength={19} // Max length including spaces (16 digits + 3 spaces)
+            type="text"
+            label={`Phone`}
+            placeholder="Enter your phone number"
+            // error={'Dsa'}
+            onInput={() => {}} // Add formatting handler
+          />
+          <>
+            <Button
+              className="mt-10 h-10 w-full"
+              variant="slim"
+              type="submit"
+              loading={loading}
+              disabled={!customer}
+              onClick={() => router.push(link_checkout_payment)}
+              //   onClick={() => setStep(1)}
+            >
+              {common_dictionary.next}
+            </Button>
+          </>
+        </div>
+        {/* {isGuest && !customer && (
           <Button
             className="mt-10"
             variant="slim"
@@ -136,9 +155,9 @@ const UserInfoView = ({ dictionary, customer }: Props) => {
             {common_dictionary.save}
           </Button>
         )} */}
-			</div>
-		</>
-	);
+      </div>
+    </>
+  );
 };
 
 export default UserInfoView;
