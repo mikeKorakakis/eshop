52be08290f628dc325dea3bc79b86c4dc1c3d084
Mike Korakakis <mike.korakakis@gmail.com>
2025-02-02 07:16:19 +0200

    fixed search bug

--- old/search.php
+++ old/search.php
@@ -14,7 +14,7 @@ include 'init.php';
 
 		// $results = getAll("SELECT * FROM products WHERE name LIKE ? OR description LIKE ?", [$searchTerm, $searchTerm]);
 		$searchTerm = '%' . $q . '%';
-		$results = getAll("SELECT products.product_id, products.name, products.description, price, media.path as image_url FROM products INNER JOIN media ON products.media_id = media.media_id WHERE products.name LIKE ? OR products.description LIKE ?", [$searchTerm, $searchTerm]);
+		$results = getAll("SELECT products.product_id, products.name, products.description, price, media.path as image_url FROM products LEFT JOIN media ON products.media_id = media.media_id WHERE products.name LIKE ? OR products.description LIKE ?", [$searchTerm, $searchTerm]);
 		
 		echo '<h1>Αποτελέσματα Αναζήτησης</h1>';
 		echo '<div id="categoryContainer">';
